7941d8e951387d29df5ae9103878a6c85efc70d0
Max Kikuchi <maxkikuchi@hotmail.com>
2023-03-15 19:40:30 -0300

    - Adição do DbContext e Models

---
+++ Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MeuTodo.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Done { get; set; }
+    }
+}
