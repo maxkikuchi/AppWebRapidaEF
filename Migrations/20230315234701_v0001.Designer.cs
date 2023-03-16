54740616a95e6a4f8aaaf8fefcb9ce19acb4b890
Max Kikuchi <maxkikuchi@hotmail.com>
2023-03-15 23:19:46 -0300

    Implementados na TodoController: - GetAsync - GetByIdAsync - PostAsync - PutAsync - DeleteAsync
    - Alterado Model Todo.cs e CreateTodoViewModel.cs

--- Migrations/20230315234701_v0001.Designer.cs
+++ Migrations/20230315234701_v0001.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using MeuTodo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MeuTodo.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230315234701_v0001")]
+    partial class v0001
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
